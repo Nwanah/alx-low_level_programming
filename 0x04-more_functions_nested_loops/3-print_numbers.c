b3e426c00a04a0057235cbf71e4deee643133526
Nwanah <nwanahleonard@gmail.com>
2023-08-18 08:15:31 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers- a function that prints the numbers, from 0 to 9
- *
+ * print_numbers - a function that prints the numbers, from 0 to 9
  * Return: print to stdout the alphabet, in lowercase, followed by a new line.
  */
 void print_numbers(void)
@@ -10,6 +9,6 @@ void print_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
-		_putchar(c);
+		_putchar(c + '0');
 	_putchar('\n');
 }		
