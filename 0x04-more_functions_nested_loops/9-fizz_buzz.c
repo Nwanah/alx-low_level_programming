bec2fa4f71b2105f86df905cb32153eda513c21b
Nwanah <nwanahleonard@gmail.com>
2023-08-19 06:10:51 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
+ * print_numbers - A program that prints the numbers from 1 to 100, followed by a new line.
  * main - Fizz Buzz
- *
  * Return: 0
-**/
+ */
 
 int main(void)
 {
@@ -14,13 +13,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i == 100)
 			printf("Buzz");
 		else if ((i % 5) == 0 && (i % 3) != 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if ((i % 3) == 0 && (i % 3) == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
