6737d870c77f39576f752139a3dc1d302fc359a4
Nwanah <nwanahleonard@gmail.com>
2023-08-21 23:17:54 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 	int i;
 	char c;
 
-	for (i= 0; s[i] !=0; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 	}
 	for (i = i - 1; i >= 0; i--)
 	{
 		c = s[i];
-	_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
